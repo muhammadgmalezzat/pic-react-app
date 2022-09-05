122545a8b9993fbdac61f5a7e8d7cfe88ebc0a42
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-09-05 14:44:16 +0200

    Search Bar added

---
+++ pictures-app/src/index.js
@@ -0,0 +1,8 @@
+//import react , react dom
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
